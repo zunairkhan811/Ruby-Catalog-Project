0252d3e0d226a9b791d16ae9df813ec632994ea0
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-07 14:42:51 +0000

    Update the function to handle cases where the JSON file has not been created

--- classes/list_book.rb
+++ classes/list_book.rb
@@ -37,8 +37,13 @@ class ListBook
       publish_date: book.publish_date,
       cover_state: book.cover_state
     }
-
-    stored_book = File.empty?('store/books.json') ? [] : JSON.parse(File.read('store/books.json'))
+  
+    if File.exist?('store/books.json') && !File.empty?('store/books.json')
+      stored_book = JSON.parse(File.read('store/books.json'))
+    else
+      stored_book = []
+    end
+  
     stored_book << obj
     File.write('store/books.json', stored_book.to_json)
   end
@@ -49,25 +54,36 @@ class ListBook
       title: label.title,
       color: label.color
     }
-
-    stored_label = File.empty?('store/labels.json') ? [] : JSON.parse(File.read('store/labels.json'))
+  
+    if File.exist?('store/labels.json') && !File.empty?('store/labels.json')
+      stored_label = JSON.parse(File.read('store/labels.json'))
+    else
+      stored_label = []
+    end
+  
     stored_label << obj
     File.write('store/labels.json', stored_label.to_json)
   end
 
   def list_all_books
-    puts 'No books added' if @books.empty?
-    @books = File.empty?('store/books.json') ? [] : JSON.parse(File.read('store/books.json'))
-    @books.each do |book|
-      puts "Publish date: #{book['publish_date']}, Publisher: #{book['publisher']}, Cover state: #{book['cover_state']}"
+    if File.exist?('store/books.json')
+      @books = JSON.parse(File.read('store/books.json'))
+      @books.each do |book|
+        puts "Publish date: #{book['publish_date']}, Publisher: #{book['publisher']}, Cover state: #{book['cover_state']}"
+      end
+    else
+      puts "The books file doesn't exist or is empty."
     end
   end
 
   def list_all_labels
-    puts 'No labels added' if @labels.empty?
-    @labels = File.empty?('store/labels.json') ? [] : JSON.parse(File.read('store/labels.json'))
-    @labels.each do |label|
-      puts "Title: #{label['title']}, Color: #{label['color']}"
+    if File.exist?('store/labels.json') && !File.empty?('store/labels.json')
+      @labels = JSON.parse(File.read('store/labels.json'))
+      @labels.each do |label|
+        puts "Title: #{label['title']}, Color: #{label['color']}"
+      end
+    else
+      puts "The labels file doesn't exist or is empty."
     end
   end
 end
